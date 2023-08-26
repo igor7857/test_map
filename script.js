7fa89775225408532001c124a6c40235b84bd721
igor7857 <igor.7857@yandex.ru>
2023-08-27 00:19:47 +0500

    1

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-const layerCheckboxes = document.querySelectorAll(".layer-checkbox");
-const layers = document.querySelectorAll(".layer");
+document.addEventListener("DOMContentLoaded", function () {
+    const subgroupContents = document.querySelectorAll(".subgroup-content");
 
-layerCheckboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("change", () => {
-        layers[index].style.display = checkbox.checked ? "block" : "none";
+    const clearCheckboxBtns = document.querySelectorAll(".disable-all-btn");
+    const showCheckboxBtns = document.querySelectorAll(".enable-all-btn");
+
+    clearCheckboxBtns.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            const checkboxes = subgroupContents[index].querySelectorAll(".layer-checkbox");
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = false;
+                checkbox.dispatchEvent(new Event("change"));
+            });
+        });
     });
 
-    // Добавляем проверку нажатых галочек при загрузке страницы
-    layers[index].style.display = checkbox.checked ? "block" : "none";
+    showCheckboxBtns.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            const checkboxes = subgroupContents[index].querySelectorAll(".layer-checkbox");
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = true;
+                checkbox.dispatchEvent(new Event("change"));
+            });
+        });
+    });
+
+    const subgroupTitles = document.querySelectorAll(".subgroup-title");
+    const toggleSubgroupBtns = document.querySelectorAll(".toggle-subgroup-btn");
+
+    toggleSubgroupBtns.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            subgroupContents[index].classList.toggle("hidden");
+        });
+    });
+
+    subgroupTitles.forEach((title, index) => {
+        title.addEventListener("click", () => {
+            subgroupContents[index].classList.toggle("hidden");
+        });
+    });
+
+    const layerCheckboxes = document.querySelectorAll(".layer-checkbox");
+    const layers = document.querySelectorAll(".layer");
+
+    layerCheckboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener("change", () => {
+            layers[index].style.display = checkbox.checked ? "block" : "none";
+        });
+
+        layers[index].style.display = checkbox.checked ? "block" : "none";
+    });
 });
+
+// Стили и другая часть вашего кода...
