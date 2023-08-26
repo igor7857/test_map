1f867cc978a1a3a5060b26beac4aba52fd75cfcd
igor7857 <igor.7857@yandex.ru>
2023-08-26 11:39:22 +0500

    1

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ layerCheckboxes.forEach((checkbox, index) => {
     checkbox.addEventListener("change", () => {
         layers[index].style.display = checkbox.checked ? "block" : "none";
     });
+
+    // Добавляем проверку нажатых галочек при загрузке страницы
+    layers[index].style.display = checkbox.checked ? "block" : "none";
 });
